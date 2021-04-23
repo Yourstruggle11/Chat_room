b59f78e0b0f96b80634c6ff764963c5adbd7effc
Yourstruggle11 <82510209+Yourstruggle11@users.noreply.github.com>
2021-04-23 14:43:24 +0530

    Update chat_room_code.js

--- chat_room_code.js
+++ chat_room_code.js
@@ -7,42 +7,76 @@ console.log(database);
 let Name = prompt("Please enter your name", "")
 
 function writeUserData() {
-    let msg = document.querySelector("#msg").value
-    database.ref('messages/').push().set({
-      message:  msg,
-      name: Name
-    });
-  }
+  let msg = document.querySelector("#msg").value
+  database.ref('messages/').push().set({
+    message: msg,
+    name: Name
+  });
+}
 
 
-  //read data and give the input to the ui
+//read data and give the input to the ui
 
-database.ref('messages' ).on('child_added', (snapshot) => {
+database.ref('messages').on('child_added', (snapshot) => {
   let msgtoshow = snapshot.val();
   console.log(snapshot.key)
 
-   /*for(let key in msgtoshow){
-       var showthemsg = msgtoshow[key];
-       
-   }*/
-   var x = document.createElement("P");                        
-   var t = document.createTextNode(`${msgtoshow.name}: ${msgtoshow.message}`);    
-    x.appendChild(t);                                           
-    document.getElementById('showmsg').appendChild(x)
-    var msgscroll = document.querySelector("#showmsg");
+  /*for(let key in msgtoshow){
+      var showthemsg = msgtoshow[key];
+      
+  }*/
+
+  if (Name == msgtoshow.name) {
+    var figure = document.createElement("figure");
+    figure.setAttribute("class", "right");
+    var image = document.createElement("img");
+    image.id = "Id";
+    image.className = "class";
+    image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpdX6tPX96Zk00S47LcCYAdoFK8INeCElPeJrVDrh8phAGqUZP_g";
+    figure.appendChild(image)
+    var x = document.createElement("P");
+    var t = document.createTextNode(` ${msgtoshow.message}`);
+    x.appendChild(t);
+    //x.appendChild(figure) 
+    document.getElementById('showmsg').appendChild(figure)
+    figure.appendChild(x)
+    // document.getElementById('showmsg').appendChild(x);
+
+  }
+  else {
+    var figure = document.createElement("figure");
+    figure.setAttribute("class", "left");
+    var image = document.createElement("img");
+    image.id = "Id";
+    image.className = "class";
+    image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpdX6tPX96Zk00S47LcCYAdoFK8INeCElPeJrVDrh8phAGqUZP_g";
+    figure.appendChild(image)
+    var x = document.createElement("P");
+    var t = document.createTextNode(`${msgtoshow.message}`);
+    x.appendChild(t);
+    //x.appendChild(figure) 
+    document.getElementById('showmsg').appendChild(figure)
+    figure.appendChild(x)
+    // document.getElementById('showmsg').appendChild(x);
+  }
 
 
-    autoscrool();
-    function autoscrool() {
-      msgscroll.scrollTop = msgscroll.scrollHeight;
-    }
-   // console.log(t);
+
+
+  var msgscroll = document.querySelector("#showmsg");
+
+
+  autoscrool();
+  function autoscrool() {
+    msgscroll.scrollTop = msgscroll.scrollHeight;
+  }
+  // console.log(t);
 });
 //use enter key to send msg
 
-document.addEventListener("keypress", function(e){
+document.addEventListener("keypress", function (e) {
   //console.log(e.key);
-  if(e.key === "Enter"){
+  if (e.key === "Enter") {
     writeUserData();
   }
 })
@@ -56,10 +90,9 @@ document.addEventListener("keypress", function(e){
     var messageId = self.getAttribute("data-id");
     database.ref("messages").child(messageId).remove();
   }*/
-  /*
-  document.getElementById("del").addEventListener("click", function(){
-    database.ref('messages/').remove()
-  })*/
+document.getElementById("del").addEventListener("click", function () {
+  database.ref('messages/').remove()
+})
 
 
 
