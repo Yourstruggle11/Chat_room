eac337ece1cebb25ae73b176fd88746933ca597e
Yourstruggle11 <82510209+Yourstruggle11@users.noreply.github.com>
2021-04-24 23:13:25 +0530

    Update chat_room_code.js

--- chat_room_code.js
+++ chat_room_code.js
@@ -7,6 +7,7 @@ console.log(database);
 let Name = prompt("Please enter your name", "")
 
 function writeUserData() {
+
   let msg = document.querySelector("#msg").value
   database.ref('messages/').push().set({
     message: msg,
@@ -17,23 +18,16 @@ function writeUserData() {
 }
 
 
+
 //read data and give the input to the ui
 
 database.ref('messages').on('child_added', (snapshot) => {
   let msgtoshow = snapshot.val();
   console.log(snapshot.key)
-
   /*for(let key in msgtoshow){
       var showthemsg = msgtoshow[key];
       
   }*/
-
-    //set msg time
-  var time = new Date();
-  var hrs = time.getHours();
-  var min = time.getMinutes()
-
-console.log(`${hrs}${min}`)
 
 
 
@@ -46,18 +40,37 @@ console.log(`${hrs}${min}`)
     image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpdX6tPX96Zk00S47LcCYAdoFK8INeCElPeJrVDrh8phAGqUZP_g";
     figure.appendChild(image)
     var x = document.createElement("P");
+    x.setAttribute("id", `m-${snapshot.key}`);
     var t = document.createTextNode(` ${msgtoshow.message}`);
     x.appendChild(t);
     //x.appendChild(figure) 
     document.getElementById('showmsg').appendChild(figure)
     figure.appendChild(x)
-    var pForTime = document.createElement("p");
-    var messageTime = document.createTextNode(`${hrs}:${min}`)
-    pForTime.appendChild(messageTime);
-    pForTime.setAttribute("class", "timeParaRight")
-    document.getElementById('showmsg').appendChild(pForTime);
+    //set msg time
+      let time = new Date();
+      let hrs = time.getHours();
+      let min = time.getMinutes();
+      //console.log(`${hrs}${min}`)
+      var pForTime = document.createElement("p");
+      var messageTime = document.createTextNode(`${hrs}:${min}`)
+      pForTime.appendChild(messageTime);
+      pForTime.setAttribute("class", "timeParaRight")
+      document.getElementById('showmsg').appendChild(pForTime);
+      //
+
     // document.getElementById('showmsg').appendChild(x);
 
+    //make del button
+    var delBtn = document.createElement("button");
+    delBtn.setAttribute("onclick", "deleteMsg(this)");
+    delBtn.setAttribute("data-id", snapshot.key);
+    delBtn.setAttribute("id", `message-${snapshot.key}`);
+    delBtn.setAttribute("class", "delBtn");
+    x.appendChild(delBtn);
+
+    document.getElementById(`message-${snapshot.key}`).innerHTML = "del";
+
+   
   }
   else {
     var figure = document.createElement("figure");
@@ -68,18 +81,26 @@ console.log(`${hrs}${min}`)
     image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpdX6tPX96Zk00S47LcCYAdoFK8INeCElPeJrVDrh8phAGqUZP_g";
     figure.appendChild(image)
     var x = document.createElement("P");
+    x.setAttribute("id", `m-${snapshot.key}`);
     var t = document.createTextNode(`${msgtoshow.name}: ${msgtoshow.message}`);
     x.appendChild(t);
     //x.appendChild(figure) 
     document.getElementById('showmsg').appendChild(figure)
     figure.appendChild(x)
-    var pForTime = document.createElement("p");
-    var messageTime = document.createTextNode(`${hrs}:${min}`)
-    pForTime.appendChild(messageTime);
-    pForTime.setAttribute("class", "timeParaLeft")
-    document.getElementById('showmsg').appendChild(pForTime);
+    //set msg time
+      let time = new Date();
+      let hrs = time.getHours();
+      let min = time.getMinutes();
+     // console.log(`${hrs}${min}`)
+      var pForTime = document.createElement("p");
+      var messageTime = document.createTextNode(`${hrs}:${min}`)
+      pForTime.appendChild(messageTime);
+      pForTime.setAttribute("class", "timeParaLeft")
+      document.getElementById('showmsg').appendChild(pForTime);
+      //
     // document.getElementById('showmsg').appendChild(x);
   }
+  //document.querySelectorAll(".delBtn").innerText = "Del";
 
 
 
@@ -93,6 +114,11 @@ console.log(`${hrs}${min}`)
   }
   // console.log(t);
 });
+
+
+
+
+
 //use enter key to send msg
 
 document.addEventListener("keypress", function (e) {
@@ -116,9 +142,16 @@ document.getElementById("del").addEventListener("click", function () {
   database.ref('messages/').remove()
 })
 
+firebase.database().ref("messages/").on("child_removed", function (snapshot) {
+  document.getElementById(`m-${snapshot.key}`).innerHTML = "This message has been deleted";
+});
 
 
 
- 
+ //create a function to del the msg
 
-
+ function deleteMsg(self){
+  var messageId = self.getAttribute("data-id");
+  console.log(messageId);
+  database.ref('messages/').child(messageId).remove()
+}
